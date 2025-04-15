0dad11ce0779c6d51bea2750dea1b775b19f1fe0
Jonathan Feldman <yoni101087@gmail.com>
2025-04-15 10:24:18 +0000

    typo

--- terraform/ssm/main.tf
+++ terraform/ssm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "token" {
-  name        = "${var.environment}-token"
+  name        = "token"
   description = "Token for Microservice authentication"
   type        = "SecureString"
   value       = var.token_value
