4cd6681d9af76bbc216395371e77b537f01d173d
Jonathan Feldman <yoni101087@gmail.com>
2025-04-15 08:39:45 +0000

    wrong dupplication

--- terraform/cloudwatch/main.tf
+++ terraform/cloudwatch/main.tf
@@ -8,12 +8,3 @@ resource "aws_cloudwatch_log_group" "app2" {
   retention_in_days = var.retention_in_days
 }
 
-output "app1_log_group_name" {
-  description = "CloudWatch Log Group name for app1"
-  value       = aws_cloudwatch_log_group.app1.name
-}
-
-output "app2_log_group_name" {
-  description = "CloudWatch Log Group name for app2"
-  value       = aws_cloudwatch_log_group.app2.name
-}
