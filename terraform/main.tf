6affd2726f17b8664662dc580fc773af01ae9ede
Jonathan Feldman <yoni101087@gmail.com>
2025-04-15 06:28:58 +0000

    elb

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,10 @@ module "vpc" {
 
   name                = "vpc"
   cidr                = "10.0.0.0/16"
-  availability_zones = ["us-west-2a"]
+  availability_zones  = ["us-west-2a", "us-west-2b"]  
 
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets      = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
