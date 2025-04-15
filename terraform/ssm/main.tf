a32aef3827174b732521a2289b601f88f674087a
Jonathan Feldman <yoni101087@gmail.com>
2025-04-15 10:21:04 +0000

    update ssm

--- terraform/ssm/main.tf
+++ terraform/ssm/main.tf
@@ -1,8 +1,3 @@
-resource "random_password" "token" {
-  length  = 32
-  special = true
-}
-
 resource "aws_ssm_parameter" "token" {
   name        = "token"
   description = "Token for app authentication"
